a405b499f5c15baffc31461b88b71ecc7bc5b01e
humayunkamal <kamalhumayun320@gmail.com>
2025-02-24 13:45:32 +0500

    Low mb heroBackground video added and herobanner removed

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,5 +1,4 @@
 import heroBackground from "./heroBackground.mp4";
 import whiteLogo from "./whiteLogo.png";
-import hero_banner from "./hero_banner.jpg";
 
-export { heroBackground, whiteLogo, hero_banner };
+export { heroBackground, whiteLogo };
